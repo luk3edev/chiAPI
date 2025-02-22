c708d9609008b9f1458f162c378ed625b2f08021
luk3edev <lukas.grinius.dev@proton.me>
2025-02-22 21:29:31 +0200

    git reset

--- models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type User struct {
-	ID        string `json:"id" gorm:"primaryKey"`
-	FirstName string `json:"first_name" gorm:"type:varchar(100)"`
-	LastName  string `json:"last_name" gorm:"type:varchar(100)"`
-}
